519f8bf166c25a59ff90079c729ce5b2d12cc2e9
Guruprasath <guruprasath@jarvisml.com>
2023-03-12 22:54:05 +0530

    Added Cors

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors"); // Import the CORS middleware
 const {
   createUser,
   loginUserController,
@@ -14,16 +15,17 @@ const {
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
+router.use(cors()); // Use the CORS middleware for all routes
+
 router.post("/register", createUser);
 router.post("/login", loginUserController);
 router.get("/allUsers", getAllUsers);
 router.get("/logout", logout);
-router.get("/refresh",handleRefreshTokens)
+router.get("/refresh", handleRefreshTokens);
 router.get("/:id", authMiddleware, isAdmin, getUser);
 router.delete("/:id", deleteUser);
 router.put("/updateUser", authMiddleware, updateUser);
 router.put("/blockUser/:id", authMiddleware, isAdmin, blockUser);
 router.put("/unBlockUser/:id", authMiddleware, isAdmin, unBlockUser);
-
 
 module.exports = router;
