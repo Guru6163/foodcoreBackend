0e44c74bb53ab6ab74dfff44e41d36a9e9a2cdc1
Guruprasath <guruprasath@jarvisml.com>
2023-03-12 23:03:44 +0530

    Any

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -15,7 +15,7 @@ const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // Update with your client-side domain
+  res.header("Access-Control-Allow-Origin", "*"); // Update with your client-side domain
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
